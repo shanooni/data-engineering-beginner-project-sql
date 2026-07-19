3811e73533e18d06a794b4c851fd7b41fdcfcf09
Shanoon Issaka <shanoonissaka@Shanoons-MacBook-Pro.local>
2026-07-19 08:19:12 +0000

    DE-201 comparison operation

--- 05-data-filter/comparison.sql
+++ 05-data-filter/comparison.sql
@@ -2,6 +2,32 @@
 
 \c MyDatabase;
 
--- TASK 1: selct all customers from germany
+-- EQUALS (=)
+-- TASK 1: retrieve all customers from germany
 SELECT * FROM customers
 WHERE country = 'Germany';
+
+-- NOT EQUALS (<> OR !=)
+-- TASK 2: retrieve all customers not from germany.
+SELECT * FROM customers
+WHERE country <> 'Germany';
+
+-- GREATER THAN (>)
+-- TASK 3 : Select all customer with score greater than 500
+SELECT * FROM customers
+WHERE score > 500;
+
+-- GREATER THAN OR EQUAL TO (>=)
+-- TASK 4 : retreive customer with score of 500 or more
+SELECT * FROM customers
+WHERE score >= 500;
+
+-- LESS THAN (<)
+-- TASK 5 : retrieve all customers with scores less than 500
+SELECT * FROM customers
+WHERE score < 500;
+
+-- LESS THAN OR EQUAL TO (<=)
+-- TASK 6 : retrieve all customers with scores less than or equal to 500
+SELECT * FROM customers
+WHERE score <= 500;
